2cacba8dad044d829381bf52222f3253d292636f
diegpo <dision_1@hotmail.com>
2026-01-19 13:44:21 -0300

    Projeto Django base com auth, sqlite e env

---
+++ accounts/views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect("dashboard")
+
+        return render(request, "login.html", {"error": "Login ou senha inválidos"})
+
+    return render(request, "login.html")
+
+@login_required
+def dashboard(request):
+    return render(request, "dashboard.html")
---
+++ setup/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect
+
+urlpatterns = [
+    path("", lambda request: redirect("login")),
+    path("admin/", admin.site.urls),
+    path("", include("accounts.urls")),
+]
