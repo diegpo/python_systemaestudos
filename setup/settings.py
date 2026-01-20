4d0e18cef8a30130b484cddc05aa06cdc783e407
diegpo <dision_1@hotmail.com>
2026-01-20 13:43:24 -0300

    Fix STATIC_ROOT for Render deploy

--- setup/settings.py
+++ setup/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -107,6 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Auth redirects
 LOGIN_URL = "/login/"
